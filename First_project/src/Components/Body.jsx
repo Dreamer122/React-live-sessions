96a408c8a68ecdf12976688d28d4cfeff41f1217
Dreamer122 <infotechdreamer@gmail.com>
2025-03-17 17:57:14 +0530

    useeffect and search functionality

--- First_project/src/Components/Body.jsx
+++ First_project/src/Components/Body.jsx
@@ -1,69 +1,92 @@
-import { Card } from "./Card"
-import {Restaurant_list} from "../constants"
-import { use, useState } from "react"
-const Body=()=>{
-    const [appname,setAppname]=useState("app name")
-    const [restaurant_list,setRestaurant_list]=useState(Restaurant_list)
-    const [fontsize,setFontSize]=useState(40)
+import { Card } from "./Card";
+import { Restaurant_list } from "../constants";
+import { useState, useEffect } from "react";
 
-//[variable,function]
-    const styleObj={
-        display:"flex",
-        justifyContent:"space-evenly",
-      }
 
-     // let myappname="food app"
+const Body = () => {
+  const [appname, setAppname] = useState("app name");
+  const [restaurant_list, setRestaurant_list] = useState([]);
+  const [filtered_data, setFilteredData] = useState([]);
+  const [fontsize, setFontSize] = useState(40);
 
-    //   const changename=()=>{
-    //     // myappname="Zomato"
-    //     // console.log(myappname)
-    //    setAppname("my new app name")
+  // search state
+  const [searchText, setSearchText] = useState("");
 
-    //   }
+  const styleObj = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexWrap:"wrap"
+  };
 
-      //hooks -> its just a function -> useState
+  
+  //  filter function
 
-    return(
-       <>
-       <h2 style={
-        {
-          fontSize:"50px",
-          color:"red"
-        }
-       }>Restaurant list</h2>
-      
+  const filter_values = () => {
+    const filtered_restaurants = restaurant_list.filter((value, i) => {
+      return value.title
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+    });
+
+    setFilteredData(filtered_restaurants);
+  };
+
+  const callapi= async ()=>{
+   const response= await fetch("https://dummyjson.com/products")
+   const data=await response.json()
+    console.log(data)
+    setRestaurant_list(data.products)
+    setFilteredData(data.products)
+    
+  }
+
+  useEffect(
+    ()=>{
+      callapi()
+    }
+  ,[])
+
+console.log("rendered")
+
+  return (
+    <>
+      <h2
+        style={{
+          fontSize: "50px",
+          color: "red",
+        }}
+      >
+        Restaurant list
+      </h2>
+
       {/* 
       // search bar 
       */}
 
-      <input type="search" />
-      <button>search</button>
-       <div style={styleObj}>
-       
-
-       {
-        //  unique id  >>>> index 
-        restaurant_list.map((item,index)=>{
-            return (
-                <Card restaurant={item} key={item.id} />
-            )
-
-        })
-       }
-       
-       </div>
-       <div>
-
-      
-       <h1 style={{fontSize:`${fontsize}px`}}>
-       {
-        appname
-       }
-       </h1>
-       <button onClick={()=>setFontSize(60)}>update font size</button>
-       <button onClick={()=>setAppname("my new app name")}>click me</button>
-       </div>
-       </>
-      )
-    }
-    export default Body
+      <input
+        type="search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button onClick={filter_values}>search</button>
+      <div style={styleObj}>
+        {
+          //  unique id  >>>> index
+          filtered_data.length == 0 ? (
+            <h1> Data not found Try something else....</h1>
+          ) : (
+            filtered_data.map((item, index) => {
+              return <Card product={item} key={item.id} />;
+            })
+          )
+        }
+      </div>
+      <div>
+        <h1 style={{ fontSize: `${fontsize}px` }}>{appname}</h1>
+        <button onClick={() => setFontSize(60)}>update font size</button>
+        <button onClick={() => setAppname("my new app name")}>click me</button>
+      </div>
+    </>
+  );
+};
+export default Body;
--- First_project/src/Components/Card.jsx
+++ First_project/src/Components/Card.jsx
@@ -1,13 +1,14 @@
 export const Card=(props)=>{
-    // console.log(props)
-    const {restaurant}=props
-    const {image,resturant_name,cuisine,rating}=restaurant
+    console.log(props)
+    const {product}=props
+    const {title,rating,thumbnail,category,price}=product
     return (
       <>
       <div className="card">
-        <img src={image} alt="burger king"  style={{height:"200px",width:"250px"}}/>
-        <h3 style={{fontSize:"25px"}}>{resturant_name}</h3>
-        <h5 style={{fontSize:"22px"}}>{cuisine}</h5>
+        <img src={thumbnail} alt="burger king"  style={{height:"200px",width:"250px"}}/>
+        <h3 style={{fontSize:"25px"}}>{title}</h3>
+        <h5>{price}</h5>
+        <h5 style={{fontSize:"22px"}}>{category}</h5>
         <p>Rating {rating} stars</p>
       </div>
       </>
